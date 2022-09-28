3e60d9280ca56bdc4f8c33a7a37dce389b800efd
KIRIYAMA Yuichi <kiriyama@KIRIYAMAs-MacBook-Air.local>
2022-09-28 20:34:19 +0900

    added addition and subtraction Makefile:-static dosent work so removed.

--- 9cc.c
+++ 9cc.c
@@ -7,9 +7,29 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    char *p = argv[1];
+
     printf(".globl	_main\n");
     printf("_main:\n");
-    printf("    mov w0, #%d\n",atoi(argv[1]));
+    printf("    mov w0, #%ld\n",strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            printf("    add w0, w0, #%ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-') {
+            p++;
+            printf("    sub w0, w0, #%ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "unexpected charactor: '%c'\n", *p);
+        return 1;
+    }
+
     printf("    ret\n");
 
 return 0;
