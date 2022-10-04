2ea57c78d80e8a6896f00c75906e20d3bb2789d9
KIRIYAMA Yuichi <kiriyama@KIRIYAMAs-MacBook-Air.local>
2022-10-04 22:46:40 +0900

    step6 unary

--- 9cc.c
+++ 9cc.c
@@ -157,6 +157,7 @@ Node *new_node_num(int val) {
 }
 
 Node *expr();
+Node *unary();
 
 Node *primary() {
     if (consume('(')) {
@@ -168,16 +169,24 @@ Node *primary() {
 }
 
 Node *mul() {
-    Node *node = primary();
+    Node *node = unary();
 
     for (;;) {
         if (consume('*'))
-            node = new_node(ND_MUL, node, primary());
+            node = new_node(ND_MUL, node, unary());
         else if (consume('/'))
-            node = new_node(ND_DIV, node, primary());
+            node = new_node(ND_DIV, node, unary());
         else
             return node;
     }
+}
+
+Node *unary() {
+    if (consume('+'))
+        return primary();
+    if (consume('-'))
+        return new_node(ND_SUB, new_node_num(0), primary());
+    return primary();
 }
 
 Node *expr() {
